b31e9d6fb1437ba0b27512982f3ede79e0e402a7
deanang <ngeekeng@gmail.com>
2017-07-30 02:27:32 +0800

    Drought
    Added

--- StarterSet2/Drought/drought.py
+++ StarterSet2/Drought/drought.py
@@ -1,11 +1,11 @@
 with open('rainin.txt', 'r') as I:
     n, c = [int(x) for x in I.readline().split()]
-    print(n, c)
+    sum = 0
+    with open('rainout.txt', 'w') as O:
+        for i in range(n):
+            sum += int(I.readline())
+            if sum >= c:
+                O.write(str(i+1))
+                break
+    O.close()
 I.close()
-
-
-
-
-with open('rainout.txt', 'w') as O:
-    pass
-O.close()
