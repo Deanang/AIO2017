e94c7999e65ddc7742737750b942f5839e02e48c
deanang <ngeekeng@gmail.com>
2017-08-10 02:31:54 +0800

    Added comments to Art Sculpture
    Added comments to Art Sculpture

--- AIOInt2016/sculpture/art.py
+++ AIOInt2016/sculpture/art.py
@@ -2,25 +2,33 @@ I = open("artin.txt", "r")
 O = open("artout.txt", "w")
 
 N = int(I.readline()) #Read input line by line
-D = [int(x) for x in I.read().split()]
+D = [int(x) for x in I.read().split()] #Reading all data at once
+#Split the data into time, width and height lists(arrays)
 T = [D[i] for i in range(0, len(D), 3)]
 W = [D[i] for i in range(1, len(D), 3)]
 H = [D[i] for i in range(2, len(D), 3)]
 
 ans = H[0]
-SH = [H[0]]
-L = [T[0]+W[0]-1]
+stackHeight = [H[0]]
+extension = [T[0]+W[0]-1]
 for i in range(1, N):
-    while L!=[] and T[i] > L[-1]:
-        L.pop()
-        SH.pop()
-    if L:
-        SH.append(SH[-1]+H[i])
+    #Check the next available platform level if there is a drop.
+    while extension!=[] and T[i] > extension[-1]:
+        #Discard most recent extension and stackHeight info if T[i] is greater.
+        extension.pop()
+        stackHeight.pop()
+    #Out of the while loop, we have found a platform level to catch the dropping block.
+    #Re-accumulate the height of the stack
+    if extension:
+        stackHeight.append(stackHeight[-1]+H[i])
+    #Here, none of the extension catches the block.
+    #It drops to the ground and we start again.
     else:
-        SH.append(H[i])
-    ans = max(ans, SH[-1])
-    print()
-    L.append(T[i]+W[i]-1)
+        stackHeight.append(H[i])
+    #ans keep tracks of the maximum of all stack heights.
+    ans = max(ans, stackHeight[-1])
+    #Append the next extension after the block dropped.
+    extension.append(T[i]+W[i]-1)
 
 print(ans)
 O.write(str(ans))
