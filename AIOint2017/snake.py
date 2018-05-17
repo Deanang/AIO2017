e004cacd82cb4e79134e1577c09aae0569eaac88
Dean Ang <ngeekeng@gmail.com>
2018-05-17 08:06:04 +0800

    Add files via upload

--- AIOint2017/snake.py
+++ AIOint2017/snake.py
@@ -1,71 +1,25 @@
-I, O = open('snakein.txt', 'r'), open('snakeout.txt', 'w')
-x, y = [int(x) for x in I.readline().split()]
+with open("snakein.txt", "r") as I:
+    x, y = (int(x) for x in I.readline().split())
+    a, b, D, ans = 0, 0, "N", ""
+    d = lambda a, b: abs(a-x)+abs(b-y)
+    def left(x, y, D):
+      if D == "N": return (x-1, y, "W")
+      if D == "W": return (x, y-1, "S")
+      if D == "S": return (x+1, y, "E")
+      if D == "E": return (x, y+1, "N")
+    def right(x, y, D):
+      if D == "N": return (x+1, y, "E")
+      if D == "W": return (x, y+1, "N")
+      if D == "S": return (x-1, y, "W")
+      if D == "E": return (x, y-1, "S")
+    while(d(a, b)!=0):
+        statusleft, statusright = left(a, b, D), right(a, b, D)
+        if d(statusleft[0], statusleft[1]) < d(statusright[0], statusright[1]):
+            ans += "L"
+            a, b, D = left(a, b, D)
+        else:
+            ans += "R"
+            a, b, D = right(a, b, D)
 
-class Snake(object):
-    def __init__(self):
-        #Initialize the 4 attributes of a snake, i.e (x, y), face and dist.
-        self.x = 0
-        self.y = 0
-        self.face = 'N'
-        self.dist = abs(self.x-x)+abs(self.y-y)
-    #Method to calculate the Mahattan dist of the point on the left
-    def leftDist(self):
-        a, b = self.x, self.y
-        if self.face == 'N':
-            a -= 1
-        elif self.face == 'E':
-            b -= 1
-        elif self.face == 'S':
-            a += 1
-        else:
-            b += 1
-        return abs(x-a)+abs(y-b)
-    #Method to calculate the Mahattan dist of the point on the right
-    def rightDist(self):
-        a, b = self.x, self.y
-        if self.face == 'N':
-            a += 1
-        elif self.face == 'E':
-            b += 1
-        elif self.face == 'S':
-            a -= 1
-        else:
-            b -= 1
-        return abs(x-a)+abs(y-b)
-    def moveLeft(self):
-        F = ['N', 'E', 'S', 'W']
-        i = F.index(self.face)
-        if self.face == 'N':
-            self.x -= 1
-        elif self.face == 'E':
-            self.y -= 1
-        elif self.face == 'S':
-            self.x += 1
-        else:
-            self.y += 1
-        self.dist = abs(self.x-x)+abs(self.y-y)
-        self.face = F[(i+1)%4]
-    def moveRight(self):
-        F = ['N', 'E', 'S', 'W']
-        i = F.index(self.face)
-        if self.face == 'N':
-            self.x += 1
-        elif self.face == 'E':
-            self.y += 1
-        elif self.face == 'S':
-            self.x -= 1
-        else:
-            self.y -= 1
-        self.dist = abs(self.x-x)+abs(self.y-y)
-        self.face = F[(i-1)%4]
-
-ans = ''
-Dean = Snake()
-while(Dean.dist != 0):
-    if Dean.leftDist() <= Dean.rightDist():
-        Dean.moveLeft()
-        ans += 'L'
-    else:
-        Dean.moveRight()
-        ans += 'R'
-O.write(str(ans))
+with open("snakeout.txt", "w") as O:
+    O.write(ans)
